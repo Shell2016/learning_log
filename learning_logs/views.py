e4da8265d1ea8b5ba279e3c777d625151ef1dcf1
Shell2016 <bvg2003@bk.ru>
2020-05-12 00:42:17 +0300

    Topic templates added

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Topic
+# from django.http import HttpResponse
 
-# Create your views here.
+
+# def index(request):
+#     return HttpResponse('<h1>Index Page</h1>')
+
+def index(request):
+    return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
