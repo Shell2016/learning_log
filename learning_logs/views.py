32639bf32e4a2b0358c08a946ce9ba318994f798
Shell2016 <bvg2003@bk.ru>
2020-05-13 00:20:21 +0300

    Added edit_entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 # from django.http import HttpResponse
 
@@ -46,6 +46,20 @@ def new_entry(request, topic_id):
             new_entry = form.save(commit=False)
             new_entry.topic = topic
             new_entry.save()
-            return redirect('learning_logs:topic', topic_id=topic_id)
+            return redirect('learning_logs:topic', topic_id=topic.id)
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
